5a2e144ee4fb8f2c6a8de19484dd18147c471d2c
udaykiran114 <udaykiransamal@gmail.com>
2023-09-16 16:54:03 +0530

    Fixed casing, code refactoring

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,16 +1,16 @@
 import random
 
 
-random_no=int(random.random()*100)
-print(random_no)
-number_of_attempts=10
-for i in range(number_of_attempts):
-    print(i)       
-    gussed=int(input("enter the no"))
-    if random_no==gussed:
-        print("correct guess")
+random_number = int(random.random()*100)
+
+max_number_of_attempts = 10
+
+for i in range(max_number_of_attempts):
+    guessed_number = int(input("Enter the number: "))
+    if random_number==guessed_number:
+        print("Correct Guess!")
         break
-    elif random_no>gussed:
-        print("gussed no is too low")
-    elif random_no<gussed:
-        print("gussed no is too high")
+    elif random_number>guessed_number:
+        print("Guessed number is too low!")
+    elif random_number<guessed_number:
+        print("Guessed number is too high!")
